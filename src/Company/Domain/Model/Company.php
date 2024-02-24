2387d7fff2b29ac9879fc3b77f948172de1169d3
Hector Ordonez <hector.javier.ordonez@awareways.com>
2024-02-24 14:20:31 +0100

    added company model and first test

---
+++ src/Company/Domain/Model/Company.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FinVista\Company\Domain\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
---
+++ tests/Feature/CompaniesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CompaniesTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_create_a_company(): void
+    {
+        // Arrange
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'name' => $this->faker->company,
+            'description' => $this->faker->text,
+            'address' => $this->faker->address,
+        ];
+
+        // Act
+        $this->post('companies', $attributes);
+
+        // Assert
+        $this->assertDatabaseHas('companies', $attributes);
+    }
+}
