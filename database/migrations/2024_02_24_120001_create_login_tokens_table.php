bbb05f24b92a0e8af4d75cb55b04a054a9aef5c9
Hector Ordonez <hector.javier.ordonez@awareways.com>
2024-02-25 12:21:40 +0100

    removed unnecessary migrations and added simplified users migrations

---
+++ database/migrations/2024_02_24_120001_create_login_tokens_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('login_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index();
+            $table->string('token', 50)->index();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('login_tokens');
+    }
+};
